f8ce39034777ee1ad03f8ce4c0fc1f191c771e4b
Junyue Wang <114094673+kinetsuou@users.noreply.github.com>
2024-07-18 16:33:07 +0800

    Add files via upload

--- EditFront/vite.config.ts
+++ EditFront/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
@@ -16,17 +17,11 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        // '/api'是代理标识，用于告诉node，url前面是/api的就是使用代理的
         ws: true,
-        target: 'http://127.0.0.1:8000', //这里填入你要请求的接口的前缀
+        target: 'https://ban2qdz2qaqbp8w5.aistudio-hub.baidu.com/api/', //这里填入你要请求的接口的前缀
         changeOrigin: true,   //是否跨域
         rewrite: (path) => path.replace(/^\/api/, '')
       }
     }
-  },
-  build: {
-    outDir: 'dist', // 构建输出目录
-    assetsDir: 'assets', // 静态资源目录
   }
 })
-
